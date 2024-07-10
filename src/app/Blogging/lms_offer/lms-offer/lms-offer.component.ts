48c505f4962725089d08e58edc5422395c1d643e
IbrahimNaserALdeen <barhoomnaser44@gmail.com>
2024-07-10 09:54:14 +0300

    LAST EDIT

--- src/app/Blogging/lms_offer/lms-offer/lms-offer.component.ts
+++ src/app/Blogging/lms_offer/lms-offer/lms-offer.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./lms-offer.component.css']
 })
 export class LmsOfferComponent {
-  whatsappNumber: string = '0795508124'; // Replace with actual number
+  whatsappNumber: string = '0782028246'; // Replace with actual number
   message: string = 'I would like to order now';
 
   get whatsappLink(): string {
