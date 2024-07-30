a00805344bfc37bc99dc956e4aa0bc110987ef9c
IbrahimNaserALdeen <barhoomnaser44@gmail.com>
2024-07-30 14:54:54 +0300

    last

--- src/app/Blogging/digital_marketing/digital_marketing-Body/digital-marketing-body/digital-marketing-body.component.ts
+++ src/app/Blogging/digital_marketing/digital_marketing-Body/digital-marketing-body/digital-marketing-body.component.ts
@@ -39,7 +39,7 @@ export class DigitalMarketingBodyComponent   implements OnInit, AfterViewInit {
         "Assessment & Support Visits"
       ],
       link: this.whatsappLink,
-      ctaText: 'Upgrade to Pro'
+      ctaText: 'Get Started'
     },
     // {
     //   heading: 'Ultimate',
@@ -66,7 +66,7 @@ export class DigitalMarketingBodyComponent   implements OnInit, AfterViewInit {
         'Marketing Campaign Strategy'
       ],
       link: this.whatsappLink,
-      ctaText: 'Go Ultimate'
+      ctaText: 'Get Started'
     }
   ];
 
